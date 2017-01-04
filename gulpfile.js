8b8e358d042f8d8219d87b334fed9a8186d16b1e
EmmanuelSW <edhgt89@hotmail.com>
2017-01-04 10:52:43 -0500

    Browsersync & Pug

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,38 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const pug = require('gulp-pug');
 const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync').create();
 
 gulp.task('homunculo',() => {
-
   gulp.src('./sass/*.sass')
   .pipe(sass({
     outputStyle: 'expanded',
     sourceComments: true
   }))
-
   .pipe(autoprefixer({
     versions: ['last 2 browsers']
   }))
-
-  .pipe(gulp.dest('./css'))
-
+  pipe(gulp.dest('./css'))
 });
+
+
+gulp.task('pug', () => {
+  gulp.src('./pug/*.pug')
+  .pipe(pug({
+    pretty: true
+  }))
+  .pipe(gulp.dest('./html/'))
+})
 
 
 gulp.task('default', () => {
   gulp.watch('./sass/*.sass', ['homunculo']);
+  gulp.watch('./pug/**/*.pug', ['pug']);
+
+  browserSync.init({
+    server: './html/'
+  });
+
+  gulp.watch('./html/*.html').on('change', browserSync.reload)
 });
